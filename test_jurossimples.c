b475685cdee63080a2dadd81e674a53d71c05cf6
TravaChipzzxk <allysonrodolfozk@gmail.com>
2026-05-26 22:36:08 -0300

    Update test_jurossimples.c

--- test_jurossimples.c
+++ test_jurossimples.c
@@ -5,11 +5,11 @@ int calculo_de_juros(int capital, int taxa, int tempo);
 int main()
 {
 
-    printf("Teste 1: %d (Esperado: 50)\n", calcular_juros(100, 50, 10));
+    printf("Teste 1: %d (Esperado: 50)\n", calculo_de_juros(100, 50, 10));
 
-    printf("Teste 2: %d (Esperado: 75)\n", calcular_juros(100, 25, 12));
+    printf("Teste 2: %d (Esperado: 75)\n", calculo_de_juros(100, 25, 12));
 
-    printf("Teste 3: %d (Esperado: 375)\n", calcular_juros(100, 75, 5));
+    printf("Teste 3: %d (Esperado: 375)\n", calculo_de_juros(100, 75, 5));
 
     printf("Criado por Allyson Rodolfo da Silva (https://github.com/TravaChipzzxk)");
 
